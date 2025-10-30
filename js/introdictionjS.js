546ea011434a6c81fa530560948f0deef40e5e86
aziz_boujaada <boujaadaaziz2911@gmail.com>
2025-10-30 23:56:14 +0100

    add varibales we need to js file

--- js/introdictionjS.js
+++ js/introdictionjS.js
@@ -1,4 +1,10 @@
-
+const form = document.getElementById("registerForm"); 
+const Name = document.getElementById("name").value.trim();
+const Email = document.getElementById("email").tirm();
+const Password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const Phone = document.getElementById("phone");
+const errorsZone = document.querySelectorAll(".error"); 
 
 
 
